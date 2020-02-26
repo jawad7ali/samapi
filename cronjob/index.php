e8be27e96303f615ae68a94d7a086a51164275b8
jawad ali <jawad7ali@gmail.com>
2020-02-26 23:21:18 +0500

    index file

--- cronjob/index.php
+++ cronjob/index.php
@@ -9,7 +9,6 @@ $from = $_SESSION['from'] ? $_SESSION['from'] : 10;
 $to = $_SESSION['from']+10;
 $_SESSION['from'] = $to;
 include_once 'process_federal_hierarchy.php';
-exit();
 include_once 'process_contract_data.php';
 include_once 'process_opportunity.php';
 include_once 'process_awards.php';
